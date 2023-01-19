6ea001a8459f50102efe912af8e963568983a2e1
Josue Matailo <jdmatailo@sudamericano.edu.ec>
2023-01-19 11:39:17 -0500

    first commit

---
+++ src/test/kotlin/com/example/facturasapi/service/InvoiceServiceTest.kt
@@ -0,0 +1,35 @@
+package com.example.facturasapi.service
+
+class InvoiceServiceTest {
+
+    @InjectMocks
+    lateinit var invoiceService: InvoiceService
+
+    @Mock
+    lateinit var invoiceRepository: InvoiceRepository
+
+    @Mock
+    lateinit var clientRepository: ClientRepository
+
+    val jsonString = File("./src/test/resources/invoice.json").readText(Charsets.UTF_8)
+    val invoiceMock = Gson().fromJson(jsonString, Invoice::class.java)
+
+    val clientMock = Client().apply {
+        id=1
+        nui="0301707030"
+        fullname="Juan"
+        address= "Ceunca"
+    }
+
+    @Test
+    fun saveInvoiceWhenIsCorrect(){
+        Mockito.`when`(clientRepository.findById(invoiceMock.clientId)).thenReturn(clientMock)
+        Mockito.`when`(invoiceRepository.save(Mockito.any(Invoice::class.java))).thenReturn(invoiceMock)
+        val response = invoiceService.save(invoiceMock)
+        Assertions.assertEquals(response.id, invoiceMock.id)
+    }
+
+
+
+}
+
